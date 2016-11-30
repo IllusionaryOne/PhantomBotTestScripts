b7255e5c9146fd0429d9c90b3822157ed2a26103
IllusionaryOne <illusionaryone@illusionaryone.com>
2016-11-30 14:08:41 -0600

    Update games-pirateGames.js

--- custom/games-pirateGames.js
+++ custom/games-pirateGames.js
@@ -14,6 +14,11 @@
  * and if so, it assumes all other win/lost messages for that game require a victim.
  * If a game does have a victim, then add pirate.game.needvictim to produce a message
  * to the user if they do not provide a victim (!game victimName).
+ *
+ * A game with a victim needs a 'needvictim' language entry, like seen below in the
+ * sample. An optional 'notself' language entry, like seen below, will not allow
+ * the sender to specify themselves as a victim. If 'notself' is not present for
+ * the command, then, the sender may specify themselves as the victim.
  */
 $.lang.register('pirate.console.loaded', 'Loaded Pirate Game Win/Loss Messages and Points');
 
@@ -36,6 +41,7 @@ $.lang.register('pirate.pirates.lost.points.1', '5');
 $.lang.register('pirate.pirates.lost.points.2', '10');
 
 $.lang.register('pirate.duel.needvictim', 'Avast! You need a person to fight!');
+$.lang.register('pirate.duel.notself', 'You cannot duel yourself!');
 $.lang.register('pirate.duel.win.1', '$1 beat $2 1');
 $.lang.register('pirate.duel.win.2', '$1 beat $2 2');
 $.lang.register('pirate.duel.lost.1', '$1 lost to $2 1');
